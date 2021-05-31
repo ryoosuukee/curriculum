0dee687124e4e013162a9447a83263329cc5177d
ryoosuukee <77524535+ryoosuukee@users.noreply.github.com>
2021-05-31 10:42:38 +0900

    Update Task1_1.java
    編集

--- JavaStudy/Task1_1.java
+++ JavaStudy/Task1_1.java
@@ -1,6 +1,6 @@
 public class Task1_1 {
 
-    public static void main(string[] args){
+    public static void main(string[] args) {
         
         int hogeInt = 50;
         string hogeString = "テスト";
