8863c58677ae1fecd8d14805a0d0c0e117d16edf
ryoosuukee <77524535+ryoosuukee@users.noreply.github.com>
2021-05-31 12:28:32 +0900

    Task1_2
    編集

--- JavaStudy/1-2/Task1_2.java
+++ JavaStudy/1-2/Task1_2.java
@@ -15,26 +15,26 @@ public class Task1_2 {
         // ①「JAPAN」、「AMERICA」、「KOREA」を要素の値（初期値）とする配列 countryを作成しなさい。
         String country[] = {"JAPAN", "AMERICA", "KOREA"};
         // ② ①で作成した配列の要素数を出力してください。
-        System.out.println("配列countryの要素数…" + country.length);
+        System.out.println(country.length);
         /* ③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
          *   1番目（先頭）の要素に 「りんご」 を代入
          *   2番目の要素に 「もも」 を代入
          *   3番目の要素に 「ぶどう」 を代入
          */
         String[] StrArray = new String[3];
-        StrArray[0] = "りんご"；
+        StrArray[0] = "りんご";
         StrArray[1] = "もも";
         StrArray[2] = "ぶどう";
-        
+
         // ④ ③で作成した配列の2番目の要素を出力しなさい。
-        System.out.println(StrArray[2]);
+        System.out.println(StrArray[1]);
         /* ⑤下記の処理について、何をしているのかコメントを記入してください。
          *  [ここへ記述]10,20,30,40,50を要素の値（初期値）とする、要素数 5 の intArray という名前のint型配列を宣言
          */
         int[] intArray = { 10, 20, 30, 40, 50 };
 
         // ⑥下記の処理について、何をしているのかコメントを記入してください。
-        // [ここへ記述]intArrayの 2 番目の要素と 4 番目の要素を出力しなさい
+        // [ここへ記述]intArrayの 2 番目の要素と 5 番目の要素の合計を出力しなさい
         System.out.println(intArray[1] + intArray[4]);
 
     }
