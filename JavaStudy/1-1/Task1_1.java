e88397df4678ce38d19e48bd4aa84198656ca0e9
ryoosuukee <77524535+ryoosuukee@users.noreply.github.com>
2021-05-31 10:48:39 +0900

    編集
    task1_1

--- JavaStudy/1-1/Task1_1.java
+++ JavaStudy/1-1/Task1_1.java
@@ -1,9 +1,9 @@
 public class Task1_1 {
 
-    public static void main{string[] args}{
+    public static void main(string[] args) {
         
         int hogeInt = 50;
-        string hogeString = "テスト";
+        String hogeString = "テスト";
 
         //double型の hogeDouble という名前の変数を宣言し、値「0.2」で初期化
         double hogeDouble = 0.2;
@@ -11,12 +11,12 @@ public class Task1_1 {
         hogeInt = 100;
         hogeString = "合格";
 
-          System.out.println(hogeInt);
+        System.out.println(hogeInt);
 
-          //変数 hogeString の値を表示
-          System.out.println(hogeString);
+        //変数 hogeString の値を表示
+        System.out.println(hogeString);
 
-          System.out.println(hogeDouble);
+        System.out.println(hogeDouble);
 
 
     }
