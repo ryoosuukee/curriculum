2eb30f87360113c141b67bdf0226ee66cd20442d
ryoosuukee <77524535+ryoosuukee@users.noreply.github.com>
2021-06-09 07:38:20 +0900

    check

--- JavaStudy/Check1/Check.java
+++ JavaStudy/Check1/Check.java
@@ -25,9 +25,9 @@ public class Check{
                 * 以下、配列の添字を入れてソートを完成させなさい
                 */
                 if(data[0] > data[1]){
-                  int box = data[1];
-                  data[1] = data[i];
-                  data[i] = box;
+                  int box = data[0];
+                  data[0] = data[2];
+                  data[2] = box;
                 }
             }
         }
