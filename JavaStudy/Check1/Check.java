fdf7c3d33060822ca1757c2a1449a21e33113076
ryoosuukee <77524535+ryoosuukee@users.noreply.github.com>
2021-06-15 07:41:16 +0900

    check

--- JavaStudy/Check1/Check.java
+++ JavaStudy/Check1/Check.java
@@ -9,7 +9,7 @@ public class Check{
         * 問1
         * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
         */
-        int data[] = {3, 1, 2, 7, 5};
+        int[] data = {3, 1, 2, 7, 5};
         /*
         * 問2
         * 以下のfor文を完成させなさい
@@ -18,16 +18,16 @@ public class Check{
             System.out.print(data[i] + " ");
         }
         System.out.println();
-        for (int i = 0; i < 5; i++) {
-            for (int j = i +1; j > i; j--) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 4 - i; j > i; j--) {
                 /*
                 * 問3
                 * 以下、配列の添字を入れてソートを完成させなさい
                 */
-                if(data[j] > data[i]){
+                if(data[j] > data[j + i]){
                   int box = data[j];
-                  data[j] = data[i];
-                  data[i] = box;
+                  data[j] = data[j + 1];
+                  data[j + 1] = box;
                 }
             }
         }
