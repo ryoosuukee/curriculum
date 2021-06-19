94d1553a34cc721e88142f624b6245b624f3b9fe
ryoosuukee <77524535+ryoosuukee@users.noreply.github.com>
2021-06-19 13:50:44 +0900

    main

--- JavaStudy/2-1/src/study/Main.java
+++ JavaStudy/2-1/src/study/Main.java
@@ -1,7 +1,5 @@
 package study;
 
-import com.sun.beans.introspect.PropertyInfo.Name;
-
 /**
  *
  * 本課題では、フィールド変数、メソッドの基本的な使い方を学んでいきましょう。
@@ -17,10 +15,11 @@ public class Main {
       //① firstNameとlastNameという名前の変数を定義し、
       //   firstNameには自分の名前、lastNameには自分の名字で初期化しなさい
 
-        String firstName = "Ryosuke";
-        String lastName = "Suzuki";
-      
-
+    	
+    	String firstName = "Ryosuke";
+    	String lastName = "Suzuki";
+    			
+    
         //getName関数の呼び出しと出力
         System.out.println(getName(firstName, lastName));
         System.out.println();
@@ -29,24 +28,23 @@ public class Main {
 
         //③ arr 配列をループさせ、isOdd関数を使って配列の要素が奇数かどうかの判定を行いなさい。
         //   要素が奇数の場合には「nは奇数です。」と出力されます。
-
-
-        for(int i = 0; i < 11; i++) {
         
-        isOdd(int num);
-
-    }
+        for(i = 0; i<11; i++) {
+        	
+        	
 
     //② 関数を定義しなさい。
     //   関数名：getName
     //  修飾子：public
     //   引数には①で定義したfirstNameとlastNameを引数で受け取り、
     //  last_name と first_name を連結した値を返り値とする変数を定義しなさい。
-
-    public static int getName(firstName, lastName) {
-    	Name = "firstName" + "lastName";
-    	return name;
+    
+    
+    public static String getName(String firstName, String lastName) {
+    
+    return ("firstName" + "lastName"); 
     }
+
 
 
     //奇数の判定を行う。
