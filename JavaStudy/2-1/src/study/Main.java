78c1b0a4d206df667121321fe9b66318a18cbbf8
ryoosuukee <77524535+ryoosuukee@users.noreply.github.com>
2021-07-03 15:46:11 +0900

    2-1

--- JavaStudy/2-1/src/study/Main.java
+++ JavaStudy/2-1/src/study/Main.java
@@ -40,14 +40,14 @@ public class Main {
     //  修飾子：public
     //   引数には①で定義したfirstNameとlastNameを引数で受け取り、
     //  last_name と first_name を連結した値を返り値とする変数を定義しなさい。
-    }
+    
     
     public static String getName(String firstName, String lastName) {
     	
     	return "firstName" + "lastName";
      }
   
-
+    }
 
     //奇数の判定を行う。
     public static void isOdd(int num) {
