2538019d6e3f704740bd1888356e4ca2d5cc8eda
ryoosuukee <77524535+ryoosuukee@users.noreply.github.com>
2021-07-03 17:29:28 +0900

    2-2

--- JavaStudy/2-2/src/study/Main.java
+++ JavaStudy/2-2/src/study/Main.java
@@ -1,10 +1,34 @@
 package study;
 
+/**
+ * Main.java
+ *
+ * 本課題では、mainメソッド・インスタンス・メソッドの呼び出しの使い方を学んでいきましょう。
+ * 課題範囲は以下のとおりです。
+ *   User.java: 問①
+ *   Main.java: 問①から問②
+ * 指定された値と変数名を守って記述してください。
+ *
+ */
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+    public static void main(String[] args) {
 
-	}
+        // ① 以下のルールに従いUserクラスのインスタンスを生成してください。
+        // 変数名:「user」
 
+        // ② ①のインスタンス変数より、アカウント情報をコンソール出力するメソッドを呼び出しなさい。
+
+    	User AccountInfo = new User();
+    
+    	AccountInfo.name();
+    	AccountInfo.ID();
+    	AccountInfo.PW();
+    	
+    	System.out.println();
+    	
+    
+    	
+    	
+    }
 }
--- JavaStudy/2-2/src/study/User.java
+++ JavaStudy/2-2/src/study/User.java
@@ -1,10 +1,36 @@
 package study;
 
-public class User {
+/**
+ * User.java
+ */
+class User {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+    // フィールド変数
+    private String userName;
+    private int id;
+    private String password;
 
-	}
+    // コンストラクタ
+    public User(String userName, int id, String password) {
+        this.userName = userName;
+        this.id = id;
+        this.password = password;
+    }
+
+    // ① 以下のルールに従いアカウント情報（名前、ID、パスワード）を出力するメソッドを作成してください。
+    // アクセス修飾子:「protected」
+    // メソッド名:「printAccountInfo」
+    protected void printAccountInfo() {
+    	System.out.println('ユーザー名は' + this.userName );
+    }
+    
+    protected void printAccountInfo() {
+    	System.out.println('IDは、' + this.id);
+    }
+
+    protected void printAccountInof() {
+    	System.out.println('パスワードは、' + this.password);
+    	
+    }
 
 }
