75e0ddef76e77e931ff5cbfbab4b2605eb31d58a
ryoosuukee <77524535+ryoosuukee@users.noreply.github.com>
2021-08-10 11:53:16 +0900

    3-1

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,10 +1,43 @@
 package check;
 
+
+
+
+import check.*;
+import constants.*;
+
+
 public class Check {
 
-    public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+    
+    private String firstName = "真弥";
+    private String lastName = "七海";
+    
+    public Check(String firstName, String lastName) {
+        
+        this.firstName = firstName;
+        this.lastName = lastName;
+        
+    }
 
+    
+    
+    
+    public static void main(String[] args) {
+      
+        Pet rb = new Pet("java吉", "hoge");
+        
+        rb.introduce();
+        
+        RobotPet rb2 = new RobotPet("R2D2", "ルーク");
+        
+        rb2.introduce();
+       
+        
+        
+        
+        
+        
     }
 
 }
