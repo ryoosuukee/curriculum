b5baf3633fac1e6beb0766dc7e4edbdeedc73b02
ryoosuukee <77524535+ryoosuukee@users.noreply.github.com>
2021-11-26 10:49:36 +0900

    2-17

--- PHP/2-17/index.php
+++ PHP/2-17/index.php
@@ -14,4 +14,27 @@
         }
     }
 
+?><br>
+
+<?php
+//朝４〜１１時
+//昼１１〜１７時
+//夜１７〜４時
+
+    date_default_timezone_set('Asia/Tokyo');
+    $time=intval(date('H'));
+    if(4<=$time && $time<=11){
+        echo "今{$time}時台です";
+        echo "<br>";
+        echo "おはようございます";
+    }else if(12<=$time && $time<=17 ){
+        echo "今{$time}時台です";
+        echo "<br>";
+        echo "こんにちは";
+    }else{
+        echo "今{$time}時台です";
+        echo "<br>";
+        echo "こんばんは";
+    }
+
 ?>
